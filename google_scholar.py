c6ca7d5577bfea24214c24e81bcfb473bd02e95d
Jaydeep <jd.ramani111@gmail.com>
2016-04-01 11:41:38 -0700

    add try catch to google scholar

--- google_scholar.py
+++ google_scholar.py
@@ -19,27 +19,30 @@ related_pubs = []
 fjson = open("/home/jaydeep/599hw2/related_pubs.json","w")
 count = 0
 for line in f:
-	if count > 1000:
-		break
-	count += 1
-	line = line.rstrip("\n")
-	command = "scholar.py -c 20 --author "+ line +" --csv"
-	result = subprocess.check_output(command, shell=True)
-	f1 = codecs.open(tempfile_path + "related20pubs.csv", "w","utf-8")
-	f1.write(result)
-	f1.close()
-	f1 = open(tempfile_path + "related20pubs.csv","r")
-	entry = {line:[]}
-	for pub in f1:
-		subentry = {"title":"","url":"","year":""}
-		pub = pub.split("|")
-		subentry["title"] = pub[0]
-		subentry["url"] = pub[1]
-		subentry["year"] = pub[2]
-		entry[line].append(subentry)
+	try:
+		if count > 1000:
+			break
+		count += 1
+		line = line.rstrip("\n")
+		command = "scholar.py -c 20 --author "+ line +" --csv"
+		result = subprocess.check_output(command, shell=True)
+		f1 = codecs.open(tempfile_path + "related20pubs.csv", "w","utf-8")
+		f1.write(result)
+		f1.close()
+		f1 = open(tempfile_path + "related20pubs.csv","r")
+		entry = {line:[]}
+		for pub in f1:
+			subentry = {"title":"","url":"","year":""}
+			pub = pub.split("|")
+			subentry["title"] = pub[0]
+			subentry["url"] = pub[1]
+			subentry["year"] = pub[2]
+			entry[line].append(subentry)
 
-	related_pubs.append(entry)
+		related_pubs.append(entry)
 	#break
+	except:
+		continue
 	time.sleep(10)
 		
 json.dump(related_pubs,fjson)		
