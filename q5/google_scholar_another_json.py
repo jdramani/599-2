658b4d56ea87d5be5021f9305e805d263742cadc
Jaydeep <jd.ramani111@gmail.com>
2016-04-04 19:20:40 -0700

    google scholar another json format

---
+++ q5/google_scholar_another_json.py
@@ -0,0 +1,43 @@
+
+
+
+## For each entry of author_list call google scholar api and save title, url link and year
+
+import subprocess
+import codecs
+import time
+import json
+
+author_list_path = "/home/jaydeep/599hw2/authors.txt"
+tempfile_path = "/home/jaydeep/"
+f = open(author_list_path,"r")
+related_pubs = {'name':'Authors','children':[]}
+fjson = open("/home/jaydeep/599hw2/related_pubs.json","w")
+count = 0
+for line in f:
+	try:
+		if count > 1000:
+			break
+		count += 1
+		line = line.rstrip("\n")
+		command = "scholar.py -c 20 --author "+ line +" --csv"
+		result = subprocess.check_output(command, shell=True)
+		f1 = codecs.open(tempfile_path + "related20pubs.csv", "w","utf-8")
+		f1.write(result)
+		f1.close()
+		f1 = open(tempfile_path + "related20pubs.csv","r")
+		
+		for pub in f1:
+			
+			pub = pub.split("|")
+			thispub = {"name":pub[0],"author":line,"URL":pub[1],"year":pub[2]}
+			string = json.dumps(thispub)
+			fjson.write(string)
+	#break
+	except:
+		continue
+	time.sleep(10)
+	
+
+fjson.close()
+f.close()
